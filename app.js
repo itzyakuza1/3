197dff1625b427b72b782936ddf3d48b972c56be
itzyakuza1 <142312325+itzyakuza1@users.noreply.github.com>
2024-09-23 15:18:39 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+function criaCartao(categoria, pergunta, resposta) {
+        let container
+}function criaCartao(categoria, pergunta, resposta) {
+        let container = document.getElementById('container')
+}function criaCartao(categoria, pergunta, resposta) {
+        let container = document.getElementById('container')
+        let cartao = document.createElement('article')
+        cartao.className = 'cartao'
+}function criaCartao(categoria, pergunta, resposta) {
+
+        // código omitido…
+        container.appendChild(cartao)
+}cartao.innerHTML = `
+<div class="cartao__conteudo">
+<h3>Programação</h3>
+<div class="cartao__conteudo__pergunta">
+        <p>O que é JavaScript?</p>
+</div>
+<div class="cartao__conteudo__resposta">
+        <p>O JavaScript é uma linguagem de programação</p>
+</div>
+</div>
+`
